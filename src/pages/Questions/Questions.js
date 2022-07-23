859d994a2c1a6f01441366c7c1df96a13abf7623
Kedar Babaleshwar <babaleshwarkedar@gmail.com>
2022-07-23 10:41:40 +0530

    Initial Commit

--- src/pages/Questions/Questions.js
+++ src/pages/Questions/Questions.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import useAxios from '../../hooks/useAxios'
 
 const Questions = () => {
-  return <div>Questions</div>
+  let apiUrl = `/api.php?amount=10`
+
+  const { response, loading } = useAxios({ url: apiUrl })
+  console.log(response)
+
+  return (
+    <div className='questions'>
+      <div className='questions__inner'>
+        <h2>question 1</h2>
+        <h4>this is question?</h4>
+        <div className='answers'>
+          <p>answer 1</p>
+          <p>answer 2</p>
+          <p>answer 3</p>
+          <p>answer 4</p>
+        </div>
+
+        <div className='score'>
+          <p>
+            <span>Score :</span> 2 / 6
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Questions
