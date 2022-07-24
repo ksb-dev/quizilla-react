e8de861d1bbcc1483e391067d2b674189e551cb6
Kedar Babaleshwar <babaleshwarkedar@gmail.com>
2022-07-24 05:40:00 +0530

    Initial Commit

--- src/pages/Score/Score.js
+++ src/pages/Score/Score.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const FinalScore = () => {
-  return <div>FinalScore</div>
+  return (
+    <div className='score'>
+      <div className='score__inner'>Score</div>
+    </div>
+  )
 }
 
 export default FinalScore
